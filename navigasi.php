cdacd76159a957138aa5241f87cb9f37be8fe4b6
brian772 <ratatito2@gmail.com>
2026-06-02 20:50:58 +0700

    Refactor navigation structure for consistency and improved readability

--- navigasi.php
+++ navigasi.php
@@ -1,23 +1,23 @@
 <nav class="navbar">
-    <div class="menu-icon">
-        <a href="#" onclick="openSlideMenu()"><i class="fas fa-bars"></i></a>
-    </div>
-      <ul class="navbar-nav">
-        <li><a href="home.php">Home</a></li>
-        <li><a href="siswa.php">Siswa</a></li>
-        <li><a href="prodi.php">Prodi</a></li>
-        <li class="logout">
-          <a href="logout.php">
-            <i class="fas fa-sign-out-alt"></i> Logout
-          </a>
-        </li>
-      </ul>
+  <div class="menu-icon">
+    <a href="#" onclick="openSlideMenu()"><i class="fas fa-bars"></i></a>
+  </div>
+  <ul class="navbar-nav">
+    <li><a href="home.php">Home</a></li>
+    <li><a href="siswa.php">Siswa</a></li>
+    <li><a href="prodi.php">Prodi</a></li>
+  </ul>
+  <div class="navbar-nav">
+    <a href="logout.php">
+      <i class="fas fa-sign-out-alt"></i> Logout
+    </a>
+  </div>
 </nav>
 
 <div id="side-menu" class="side-nav">
-    <a href="#" class="btn-close" onclick="closeSlideMenu()">&times;</a>
-    <a href="home.php">Home</a>
-    <a href="siswa.php">Siswa</a>
-    <a href="prodi.php">Prodi</a>
-    <a href="logout.php"><i class="fas fa-sign-out-alt"></i> Logout</a>
+  <a href="#" class="btn-close" onclick="closeSlideMenu()">&times;</a>
+  <a href="home.php">Home</a>
+  <a href="siswa.php">Siswa</a>
+  <a href="prodi.php">Prodi</a>
+  <a href="logout.php"><i class="fas fa-sign-out-alt"></i> Logout</a>
 </div>
